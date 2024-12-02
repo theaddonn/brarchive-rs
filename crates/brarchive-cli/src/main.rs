b4e99b51644b0b153675b933131b7d8d5a6d1f8c
theaddon <theaddonn@gmail.com>
2024-12-02 09:05:15 +0100

    add encoding of single files

--- crates/brarchive-cli/src/main.rs
+++ crates/brarchive-cli/src/main.rs
@@ -32,47 +32,76 @@ fn main() {
             });
             
             if exists && !out.is_dir() { 
-                error!("The output directory \"{}\" already exists", out.display());
+                error!("Output directory \"{}\" already exists", out.display());
                 exit(1);
             }
 
             if exists && !out.is_file() {
-                error!("The output file \"{}\" already exists", out.display());
+                error!("Output file \"{}\" already exists", out.display());
                 exit(1);
             }
-            
-            let read_dir = fs::read_dir(&path).unwrap_or_else(|err| {
-                error!("Failed to read directory \"{}\": {}", path.display(), &err);
+
+            let exists = fs::exists(&path).unwrap_or_else(|err| {
+                error!("Failed to check if input directory exists \"{}\": {}", path.display(), err);
                 exit(1);
             });
-
-            let mut dir_entries = BTreeMap::new();
-
-            for entry in read_dir {
-                let entry = entry.unwrap_or_else(|err| {
-                    error!("Failed to read directory entry of \"{}\": {}", path.display(), err);
-                    exit(1);
-                });
-
-                let content = fs::read_to_string(entry.path()).unwrap_or_else(|err| {
-                    error!("Failed to read file \"{}\": {}", entry.path().display(), err);
-                    exit(1);
-                });
-
-                let entry_name = entry.path();
-                let entry_name = entry_name.strip_prefix(&path).unwrap_or_else(|err| {
-                    error!("Failed to strip prefix from \"{}\": {}", entry.path().display(), err);
-                    exit(1);
-                });
-                let entry_name = entry_name.to_str().unwrap_or_else(|| {
-                    error!("Failed to convert file path to UTF-8: {}", entry.path().display());
-                    exit(1);
-                });
-
-                dir_entries.insert(entry_name.to_string(), content);
+            
+            if !exists {
+                error!("Input directory \"{}\" does not exist", path.display());
+                exit(1);
             }
 
-            let archive = brarchive::serialize(dir_entries).unwrap_or_else(|err| {
+            let entries_map = if path.is_dir() {
+                let read_dir = fs::read_dir(&path).unwrap_or_else(|err| {
+                    error!("Failed to read directory \"{}\": {}", path.display(), &err);
+                    exit(1);
+                });
+                
+                let mut dir_entries = BTreeMap::new();
+                
+                for entry in read_dir {
+                    let entry = entry.unwrap_or_else(|err| {
+                        error!("Failed to read directory entry of \"{}\": {}", path.display(), err);
+                        exit(1);
+                    });
+
+                    let content = fs::read_to_string(entry.path()).unwrap_or_else(|err| {
+                        error!("Failed to read file \"{}\": {}", entry.path().display(), err);
+                        exit(1);
+                    });
+
+                    let entry_name = entry.path();
+                    let entry_name = entry_name.strip_prefix(&path).unwrap_or_else(|err| {
+                        error!("Failed to strip prefix from \"{}\": {}", entry.path().display(), err);
+                        exit(1);
+                    });
+                    let entry_name = entry_name.to_str().unwrap_or_else(|| {
+                        error!("Failed to convert file path to UTF-8: {}", entry.path().display());
+                        exit(1);
+                    });
+
+                    dir_entries.insert(entry_name.to_string(), content);
+                }
+                
+                dir_entries
+            } else if path.is_file() { 
+                let content = fs::read_to_string(&path).unwrap_or_else(|err| {
+                    error!("Failed to read file \"{}\": {}", path.display(), &err);
+                    exit(1);
+                });
+                
+                let entry_name = path.file_name().and_then(OsStr::to_str).unwrap_or_else(|| {
+                    error!("Failed to convert file path to UTF-8: {}", path.display());
+                    exit(1);
+                }).to_string();
+                
+                BTreeMap::from([(entry_name, content)])
+            } else { 
+                error!("Input Path is neither a file nor a folder \"{}\"", path.display());
+                exit(1);
+            };
+
+            let archive = brarchive::serialize(entries_map).unwrap_or_else(|err| {
                 error!("Failed to encode directory entries \"{}\": {}", err, path.display());
                 exit(1);
             });
@@ -90,6 +119,16 @@ fn main() {
 
             info!("Beginning to decode archive \"{}\" into \"{}\"", path.display(), out.display());
 
+            let exists = fs::exists(&path).unwrap_or_else(|err| {
+                error!("Failed to check if input file exists \"{}\": {}", path.display(), err);
+                exit(1);
+            });
+
+            if !exists {
+                error!("Input file \"{}\" does not exist", path.display());
+                exit(1);
+            }
+            
             let data = fs::read(&path).unwrap_or_else(|err| {
                 error!("Failed to read archive file \"{}\": {}", path.display(), err);
                 exit(1);
@@ -106,7 +145,7 @@ fn main() {
             });
 
             if exists && !out.is_dir() {
-                error!("The output directory \"{}\" already exists and is not a directory", path.display());
+                error!("Output directory \"{}\" already exists and is not a directory", path.display());
                 exit(1);
             }
 
@@ -114,7 +153,7 @@ fn main() {
 
             if let Some(contents) = directory_contents {
                 if contents.count() > 0 || exists {
-                    error!("The output directory \"{}\" already exists and is not empty", path.display());
+                    error!("Output directory \"{}\" already exists and is not empty", path.display());
                     exit(1);
                 }
             }
